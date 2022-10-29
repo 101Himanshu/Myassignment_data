0192c9c5bedbc53c0e6fbed465120ad4471eaa54
101Himanshu <112798701+101Himanshu@users.noreply.github.com>
2022-10-29 13:21:43 +0530

    Delete SQLQuery1_Google_stocks.sql

--- SQLQuery1_Google_stocks.sql
+++
@@ -1,6 +0,0 @@
-use [Company assignment];
-
-select * from dbo.google_stocks;
-
-/* highest opeaning and lowest opening by month. */
-select Date, MAX(High) as Highest_opening, MIN(Low) as Lowest_opening from dbo.google_stocks group by Date;
