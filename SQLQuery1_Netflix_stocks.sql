4311d35076628f7905c72a977936b9dfef30e016
101Himanshu <112798701+101Himanshu@users.noreply.github.com>
2022-10-29 13:22:08 +0530

    Delete SQLQuery1_Netflix_stocks.sql

--- SQLQuery1_Netflix_stocks.sql
+++
@@ -1,9 +0,0 @@
-use [Company assignment];
-
-select * from dbo.netflix_stocks;
-
-/* standard deviation of Volume per year for Netflix. */
-select  Date,
-		AVG(SUM(Volume)) over () as avg_Vol,
-		STDEV(SUM(Volume)) over () as std_of_Vol
-from dbo.netflix_stocks Group by Date;
