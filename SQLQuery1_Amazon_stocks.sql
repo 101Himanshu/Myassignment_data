b6d2e90121590dc386cf3f3cef1e31b54017b827
101Himanshu <112798701+101Himanshu@users.noreply.github.com>
2022-10-29 13:21:20 +0530

    Delete SQLQuery1_Amazon_stocks.sql

--- SQLQuery1_Amazon_stocks.sql
+++
@@ -1,6 +0,0 @@
-use [Company assignment];
-
-Select * From amazon_stocks;
-
-/* largest daily return for Amazon */
-Select Company, Date, MAX(Daily_return) as lar_daily_return from amazon_stocks group by Company, Date;
